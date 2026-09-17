540e0be791876135d407d828d17ddc0b03c44f1b
xblspain-glitch <xblspain@gmail.com>
2026-09-17 12:23:59 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 let categoryListActive=false;
 
 window.ORACIONES_APP_CHANNEL_V1="v3";
-window.ORACIONES_APP_VERSION_V1="3.1.323";
+window.ORACIONES_APP_VERSION_V1="3.1.324";
 
 let sentListActive=false;
 
